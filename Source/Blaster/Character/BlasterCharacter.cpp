f614adbc85397b5695b6b404be31f9dbeae40900
Pierson Trimarchi <pstrimar@gmail.com>
2023-02-01 17:38:24 -0500

    Add rocket launcher weapon

--- Source/Blaster/Character/BlasterCharacter.cpp
+++ Source/Blaster/Character/BlasterCharacter.cpp
@@ -138,6 +138,7 @@ void ABlasterCharacter::MulticastElim_Implementation()
 
 	// Disable character movement
 	bDisableGameplay = true;
+	GetCharacterMovement()->DisableMovement();
 	if (Combat)
 	{
 		Combat->FireButtonPressed(false);
