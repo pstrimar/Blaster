aabb3a998bb41b232d692d0e04b6a494abcfce86
Pierson Trimarchi <pstrimar@gmail.com>
2023-02-21 15:10:08 -0500

    Pickup Spawner

--- Source/Blaster/Pickups/Pickup.h
+++ Source/Blaster/Pickups/Pickup.h
@@ -46,6 +46,12 @@ private:
 	UPROPERTY(EditAnywhere)
 	class UNiagaraSystem* PickupEffect;
 
+	FTimerHandle BindOverlapTimer;
+
+	float BindOverlapTime = 0.25f;
+
+	void BindOverlapTimerFinished();
+
 public:	
 
 };
